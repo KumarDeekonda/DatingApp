52df5e80cd0497a7ea958ed719a269d99dbf396b
Prashanth <kumardvplr@gmail.com>
2019-10-15 19:44:19 -0400

    added 3rd party components like alertifyjs, bootswatch ngx-bootstrap

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -7,31 +8,30 @@ import { AuthService } from '../_services/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
   model: any = {};
   @Output() CancelRegister = new EventEmitter();
 
-  constructor(private authservice: AuthService) { }
+  constructor(
+    private authservice: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   register() {
-    this.authservice.register(this.model).subscribe(() => {
-      console.log('Registrastion Successfull');
-      this.model = {};
-
-    }, error => {
-      console.log(error);
-
-    });
-
+    this.authservice.register(this.model).subscribe(
+      () => {
+        this.alertify.success('Registrastion Successfull');
+        this.model = {};
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
   }
 
   cancel() {
-    console.log('cancelled');
+    this.alertify.message('cancelled');
     this.CancelRegister.emit(false);
-
   }
-
 }
