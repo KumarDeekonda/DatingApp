f36fdb3d392fcca3155bf80f19cad08f61156f9a
Prashanth <kumardvplr@gmail.com>
2019-10-19 23:23:12 -0400

    memberlist componets and their resolvers and views implemented

---
+++ DatingApp-SPA/src/app/_models/user.ts
@@ -0,0 +1,18 @@
+import { Photo } from './photo';
+
+export interface User {
+  id: number;
+  username: string;
+  knownAs: string;
+  age: number;
+  gender: string;
+  created: Date;
+  lastActive: Date;
+  photoUrl: string;
+  city: string;
+  country: string;
+  interests?: string; // question mark or elvis operator
+  introduction?: string;
+  lookingFor?: string;
+  photos?: Array<Photo>;
+}
